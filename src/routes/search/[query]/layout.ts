11a89d863e8e982f825051b66e278a470e847f97
enesflow <eneskavcakar34@gmail.com>
2024-07-11 20:00:53 +0300

    feat: Update plugin.tsx with removenumbers

--- src/routes/search/[query]/layout.ts
+++ src/routes/search/[query]/layout.ts
@@ -9,32 +9,34 @@ export const onRequest: RequestHandler = async ({
 }) => {
   const query = removeNumbersInWord(params.query);
   const lower = query.toLocaleLowerCase("tr");
-  if (!query.endsWith("-")) {
-    await next();
-    return;
-  }
+
   // if the query ends with a hyphen, do this
   // this is for turkish words
   // get the last vowel, check if it's a back vowel or a front vowel (a, ı, o, u or e, i, ö, ü)
-
-  const lastVowel = lower.match(/[aeıioöuü]/gi)?.slice(-1);
-  // if it's a back vowel, add "mak" to the end
-  // if it's a front vowel, add "mek" to the end
-  if (lastVowel) {
-    const [vowel] = lastVowel;
-    const noHyphen = lower.slice(0, -1);
-    let toAdd = "";
-    if ("eiüö".includes(vowel.toLowerCase())) {
-      toAdd = "mek";
-    } else if ("aıou".includes(vowel.toLowerCase())) {
-      toAdd = "mak";
+  if (query.endsWith("-")) {
+    const lastVowel = lower.match(/[aeıioöuü]/gi)?.slice(-1);
+    // if it's a back vowel, add "mak" to the end
+    // if it's a front vowel, add "mek" to the end
+    if (lastVowel) {
+      const [vowel] = lastVowel;
+      const noHyphen = lower.slice(0, -1);
+      let toAdd = "";
+      if ("eiüö".includes(vowel.toLowerCase())) {
+        toAdd = "mek";
+      } else if ("aıou".includes(vowel.toLowerCase())) {
+        toAdd = "mak";
+      }
+      const to = `${url.origin}/search/${encodeURIComponent(noHyphen + toAdd)}/`;
+      if (url.pathname !== to) {
+        // just to be safe
+        throw redirect(301, to);
+      }
     }
-    const to = `${url.origin}/search/${encodeURIComponent(noHyphen + toAdd)}/`;
-    throw redirect(301, to);
   }
-  /* if (query !== lower) {
-		throw redirect(301, `/search/${lower}`);
-	} */ // Don't need this for now
+  if (lower !== params.query) {
+    // for example, if the query is "Enes2", redirect to "enes"
+    throw redirect(301, `/search/${lower}`);
+  }
 
   await next();
 };
