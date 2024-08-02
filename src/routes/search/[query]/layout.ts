7d6328767ec4a7222af267ee48d2c696da70587c
enesflow <eneskavcakar34@gmail.com>
2024-08-02 15:12:16 +0300

    disable caching so hard

--- src/routes/search/[query]/layout.ts
+++ src/routes/search/[query]/layout.ts
@@ -2,9 +2,10 @@ import type { RequestHandler } from "@builder.io/qwik-city";
 export const onGet: RequestHandler = async ({ cacheControl }) => {
   cacheControl(
     {
-      staleWhileRevalidate: 60 * 60 * 24 * 30,
-      maxAge: 60 * 60 * 24,
-      public: true,
+      // Always serve a cached response by default, up to a week stale
+      staleWhileRevalidate: 60 * 60 * 24 * 7,
+      // Max once every 5 seconds, revalidate on the server to get a fresh version of this page
+      maxAge: 5,
     },
     "Cloudflare-CDN-Cache-Control",
   );
