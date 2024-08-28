71015d3a7d1c4167446f0d26a62f66d38498d45d
Enes Kavçakar <eneskavcakar34@gmail.com>
2024-08-28 12:12:59 +0000

    fixable search bar on top when scrolled down

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,6 +1,7 @@
 import {
   $,
   component$,
+  useOnDocument,
   useOnWindow,
   useSignal,
   useTask$,
@@ -9,6 +10,9 @@ import { useLocation, useNavigate } from "@builder.io/qwik-city";
 import { isBrowser } from "@builder.io/qwik/build";
 import Spinner from "./spinner";
 export const SearchBar = component$<{ value?: string }>(({ value }) => {
+  const placeholderRef = useSignal<HTMLFormElement>();
+  const formRef = useSignal<HTMLFormElement>();
+  const fixToTop = useSignal(false);
   const nav = useNavigate();
   const query = useSignal(value ?? "");
   const isLoading = useSignal(false);
@@ -26,9 +30,25 @@ export const SearchBar = component$<{ value?: string }>(({ value }) => {
     track(() => loc.isNavigating);
     focusOnInput();
   });
+  useOnDocument(
+    "scroll",
+    $(() => {
+      const formTop = placeholderRef.value?.getBoundingClientRect().top ?? 0;
+      const height = formRef.value?.getBoundingClientRect().height ?? 0;
+      const fix = formTop < -(height + 16); // 16px is m-4
+      fixToTop.value = fix;
+      if (fix) {
+        placeholderRef.value?.style.setProperty("height", `${height}px`);
+      } else {
+        placeholderRef.value?.style.removeProperty("height");
+      }
+    }),
+  );
   return (
     <>
+      <div ref={placeholderRef} class="my-4" />
       <form
+        ref={formRef}
         preventdefault:submit
         onSubmit$={() => {
           if (query.value.length > 0) {
@@ -40,7 +60,7 @@ export const SearchBar = component$<{ value?: string }>(({ value }) => {
             nav("/");
           }
         }}
-        class="search-form"
+        class={`search-form ${fixToTop.value ? "search-form-fixed" : ""}`}
       >
         <input
           type="text"
