0bc953f8af8c60fb00677a922e757a300ce8cc86
Enes Kavçakar <eneskavcakar34@gmail.com>
2024-09-15 20:49:11 +0000

    some space above the cat

--- src/components/cat.tsx
+++ src/components/cat.tsx
@@ -2,11 +2,11 @@ import { component$ } from "@builder.io/qwik";
 import ImgCatup from "~/../public/images/catup.png?jsx";
 export const CatLookingUp = component$(() => {
   return (
-    <div>
+    <div class="h-72 md:h-96">
       <div class="pb-72 md:pb-96"></div>
       <ImgCatup
         alt="catup"
-        class="absolute -bottom-8 left-0 h-72 w-auto shadow-lg dark:brightness-75 md:h-96"
+        class="bottom-fix absolute left-0 h-64 w-auto shadow-lg dark:brightness-75 md:h-[22rem]"
       />
     </div>
   );
