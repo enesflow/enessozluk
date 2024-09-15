a21f83a5b9bcaf1113177749e71dca81bcdc6062
Enes Kavçakar <eneskavcakar34@gmail.com>
2024-09-15 12:10:44 +0000

    trying to add gtag

--- src/root.tsx
+++ src/root.tsx
@@ -22,9 +22,23 @@ export default component$(() => {
         <meta charset="utf-8" />
         <link rel="manifest" href="/manifest.json" />
         <RouterHead />
-        <meta name="google-site-verification" content="uQpmM8mPahytZ1sne4-rNUfBoibnJ-VDzmnsXzazZr0" />
+        <meta
+          name="google-site-verification"
+          content="uQpmM8mPahytZ1sne4-rNUfBoibnJ-VDzmnsXzazZr0"
+        />
+        <script
+          dangerouslySetInnerHTML="(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K5CNH95B');"
+        />
       </head>
       <body lang="en">
+        <noscript
+          dangerouslySetInnerHTML='<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K5CNH95B"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>'
+        />
         <RouterOutlet />
         <ServiceWorkerRegister />
       </body>
