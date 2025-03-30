ea71ff697b0684a6c5f3ba42b5d608c787f01eb4
enesflow <mail@enesin.xyz>
2025-03-30 04:23:58 +0300

    fix: add missing class name to result container in RhymeView component

--- src/components/dicts/rhyme.tsx
+++ src/components/dicts/rhyme.tsx
@@ -13,7 +13,7 @@ export const RhymeView = component$<{
           <p class="error-message">{data.value.serverDefinedError}</p>
         </>
       ) : (
-        <div class="pb-4">
+        <div class="result-subitem pb-4">
           <p class="result-title-took">75.000'den fazla kelime arasında</p>
           <WordLinks words={data.value.items} more={data.value.more} />
         </div>
